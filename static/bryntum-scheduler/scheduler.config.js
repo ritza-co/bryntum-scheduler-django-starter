f9009971b3abcf51a8be1bc327809ad1e87a9e07
Tatenda <31291528+rideam@users.noreply.github.com>
2024-06-24 13:57:49 +0200

    formatting

--- static/bryntum-scheduler/scheduler.config.js
+++ static/bryntum-scheduler/scheduler.config.js
@@ -1,32 +1,32 @@
-import { Scheduler } from "./scheduler.module.js";
+import { Scheduler } from './scheduler.module.js';
 
 const terminalHideDelay = 300;
 const terminalShowDelay = 100;
 
 const scheduler = new Scheduler({
-  appendTo: document.body,
-  viewPreset: "hourAndDay",
-  features: {
-    dependencies: {
-      clickWidth: 5,
-      radius: 10,
-      terminalOffset: 0,
-      terminalSize: 12,
-      terminalShowDelay,
-      terminalHideDelay,
+    appendTo   : document.body,
+    viewPreset : 'hourAndDay',
+    features   : {
+        dependencies : {
+            clickWidth     : 5,
+            radius         : 10,
+            terminalOffset : 0,
+            terminalSize   : 12,
+            terminalShowDelay,
+            terminalHideDelay
+        },
+        dependencyEdit : {
+            showLagField : false
+        }
     },
-    dependencyEdit: {
-      showLagField: false,
+    crudManager : {
+        loadUrl          : '/load',
+        autoLoad         : true,
+        syncUrl          : '/sync',
+        autoSync         : true,
+        // This config enables response validation and dumping of found errors to the browser console.
+        // It's meant to be used as a development stage helper only so please set it to false for production systems.
+        validateResponse : true
     },
-  },
-  crudManager: {
-    loadUrl: "/load",
-    autoLoad: true,
-    syncUrl: "/sync",
-    autoSync: true,
-    // This config enables response validation and dumping of found errors to the browser console.
-    // It's meant to be used as a development stage helper only so please set it to false for production systems.
-    validateResponse: true,
-  },
-  columns: [{ text: "Name", field: "name", width: 130 }],
+    columns : [{ text : 'Name', field : 'name', width : 130 }]
 });
